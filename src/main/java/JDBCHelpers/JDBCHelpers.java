db15042d17717d4048aafc8bb3c4af1a83b17663
A5Playz <aayushs474@gmail.com>
2021-07-26 04:51:00 +0530

    Added JDBCHelpers library

--- src/main/java/JDBCHelpers/JDBCHelpers.java
+++ src/main/java/JDBCHelpers/JDBCHelpers.java
@@ -1,14 +1,18 @@
+package JDBCHelpers;
+
+import com.sun.istack.internal.NotNull;
 import com.sun.istack.internal.Nullable;
 
 import java.sql.*;
+import java.sql.Connection;
 
 public class JDBCHelpers {
 
-    @SuppressWarnings(Unused)
-    public Connection getConnection(String url, String username, String password, @Nullable String database) {
+    public static java.sql.Connection getConnection(@NotNull String url, @NotNull String username, @NotNull String password, @Nullable String database) {
         try{
-            if(database != null) return DriverManager.getConnection(url+"/"+database, username, password);
-            else return DriverManager.getConnection(url, username, password);
+            //TODO Add support for other drivers by changing the static jdbc:mysql: string to some object
+            if(database != null) return DriverManager.getConnection("jdbc:mysql://"+url+"/"+database, username, password);
+            else return DriverManager.getConnection("jdbc:mysql://"+url, username, password);
         }catch (SQLException e){
             System.out.println("ERROR: Cannot create connection object.");
             e.printStackTrace();
@@ -16,13 +20,14 @@ public class JDBCHelpers {
         return null;
     }
 
-    public Connection getConnection(String url, String username, String password) {
+    public static java.sql.Connection getConnection(@NotNull String url, @NotNull String username, @NotNull String password) {
         return getConnection(url, username, password, null);
     }
 
-    public static ResultSet getScrollableResultSet(Connection connection){
+    public static ResultSet getScrollableResultSet(@NotNull java.sql.Connection connection, @NotNull String sqlQuery){
         try{
-            return (ResultSet) connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            return connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)
+                    .executeQuery(sqlQuery);
         }catch (SQLException e){
             System.out.println("ERROR: Cannot create scrollable result set.");
             e.printStackTrace();
@@ -30,15 +35,23 @@ public class JDBCHelpers {
         return null;
     }
 
-    public static int getRowCount(ResultSet resultSet){
+    public static ResultSetMetaData getResultSetMetaData(@NotNull ResultSet resultSet){
+        try {
+            return resultSet.getMetaData();
+        }catch (SQLException e){
+            System.out.println("ResultSet meta data cannot be fetched.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static int getRowCount(@NotNull ResultSet resultSet){
         int toReturn = -1;
         try{
-            System.out.println("Original position: "+resultSet.getRow());
             int currentPosition = resultSet.getRow();
             resultSet.last();
             toReturn = resultSet.getRow();
             resultSet.absolute(currentPosition);
-            System.out.println("New position: "+resultSet.getRow());
         }catch (SQLException e){
             System.out.println("The result set either forward only or some other error occurred.");
             e.printStackTrace();
@@ -46,25 +59,80 @@ public class JDBCHelpers {
         return toReturn;
     }
 
-    public static void printResultSetData(ResultSet resultSet){
+    public static void printResultSetData(@NotNull ResultSet resultSet, String message){
+        System.out.println(getPrintableResultSetData(resultSet, message));
+    }
+
+    public static void printResultSetData(ResultSet resultSet) {
+        printResultSetData(resultSet, null);
+    }
+
+    public static String getPrintableResultSetData(@NotNull ResultSet resultSet, @Nullable String message){
         try{
+            StringBuilder stringBuilder = new StringBuilder();
             ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
             if(columnCount > 0){
-                for(int i = 0; i < columnCount; i++){
-                    System.out.print(metaData.getColumnLabel(i)+"\t");
+                stringBuilder.append("\n").append(message);
+                stringBuilder.append("\nRow No.\t");
+                for(int i = 1; i <= columnCount; i++){
+                    stringBuilder.append(metaData.getColumnLabel(i)).append("\t");
                 }
-                System.out.println();
+                stringBuilder.append("\n");
+                int rowCounter = 0;
                 while(resultSet.next()){
-                    for(int i =0; i < columnCount; i++){
-                        System.out.print(resultSet.getString(i));
+                    stringBuilder.append(++rowCounter).append("\t");
+                    for(int i = 1; i <= columnCount; i++){
+                        stringBuilder.append(resultSet.getString(i)).append("\t");
                     }
-                    System.out.println();
+                    stringBuilder.append("\n");
                 }
+                stringBuilder.append("Total rows: ").append(getRowCount(resultSet)).append("\n");
+                stringBuilder.append("Total columns: ").append(columnCount).append("\n");
             }
+            else stringBuilder.append("\nThere are no rows to print.\n");
+            return stringBuilder.toString();
         }catch (SQLException e){
-            System.out.println("Some error occurred while printing.");
+            System.out.println("\nSome error occurred while printing.\n");
             e.printStackTrace();
         }
+        return "";
     }
+
+    public static String getPrintableResultSetData(@NotNull ResultSet resultSet){
+        return getPrintableResultSetData(resultSet, null);
+    }
+
+    public static boolean closeConnection(@NotNull java.sql.Connection connection) {
+        try{
+            connection.close();
+            return true;
+        }catch (SQLException e){
+            System.out.println("The connection was already closed or some other error occurred.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean executeDDL(Connection connection, String sqlQuery) {
+        try{
+            return connection.createStatement().execute(sqlQuery);
+        }catch (SQLException e){
+            System.out.println("Cannot execute the given query.\n"+sqlQuery+"\n");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static int executeDML(Connection connection, String sqlQuery) {
+        try{
+            return connection.createStatement().executeUpdate(sqlQuery);
+        }catch (SQLException e){
+            System.out.println("Cannot execute the given query.\n"+sqlQuery+"\n");
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
 }
--- src/main/java/JdbcBasics/Sample.java
+++ src/main/java/JdbcBasics/Sample.java
@@ -1,2 +1,51 @@
-package JdbcBasics;public class Sample {
+package JdbcBasics;
+
+import JDBCHelpers.*;
+
+public class Sample {
+    private static void runner(){
+        Connections connections = new Connections();
+        if(connections.open("localhost:3306/sem5", "root","")){
+
+            if(connections.executeDDL("CREATE TABLE `TES` (\n" +
+                    "  `ano` INT(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `holder` VARCHAR(100) DEFAULT NULL,\n" +
+                    "  `password` VARCHAR(100) DEFAULT NULL,\n" +
+                    "  `amount` FLOAT(20),\n" +
+                    "  PRIMARY KEY (`ano`)\n" +
+                    ")")){
+                System.out.println("Table created.");
+            }
+            System.out.println("Inserted "+
+                    connections.executeDML("INSERT INTO `TES` (`holder`, `password`, `amount`) \n" +
+                    "VALUES\n" +
+                    "('A5', 'password',500),\n" +
+                    "('TES', 'something',1500),\n" +
+                    "('Harshang', 'strong_password',500),\n" +
+                    "('Leo', 'wonderful_password',1500)\n" +
+                    ";") + " rows.");
+
+            if(connections.executeResult("SELECT * FROM `TES`")) {
+                System.out.println("ResultSet has been generated");
+            }
+
+            System.out.println(connections.executeDML("UPDATE `TES` SET PASSWORD = 'new_stronger_p@ssword' WHERE ano = 1;")+" rows updated.");
+
+            if(connections.executeResult("SELECT * FROM `TES`")) {
+                System.out.println("ResultSet has been generated");
+            }
+
+            if(connections.executeDDL("DROP TABLE `TES`;")){
+                System.out.println("Table dropped.");
+            }
+
+            connections.setCurrentResultSetIndex(0);
+            connections.printCurrentResultSetData("Table before updating");
+            connections.setCurrentResultSetIndex(1);
+            connections.printCurrentResultSetData("Table after updating");
+        }
+    }
+    public static void main(String[] args) {
+        Sample.runner();
+    }
 }
